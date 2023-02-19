1cb141e0f9bce2a2ea7545b74b1018a7bf6dc994
Anton <Antosah1@mail.ru>
2023-02-19 11:53:11 +0300

    init commit

---
+++ src/main/java/ru/nataliaoskina/fhhbot/FhhBotApplication.java
@@ -0,0 +1,13 @@
+package ru.nataliaoskina.fhhbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhhBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhhBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nataliaoskina/fhhbot/FhhBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nataliaoskina.fhhbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhhBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
